62860e85fdff7f6bd275f9136271d9fad5838301
haidernafees93 <haidernafees93@gmail.com>
2020-03-25 13:11:25 -0400

    Solved Stack Min problem and added file of stackMin.java

--- runner.java
+++ runner.java
@@ -1,19 +1,15 @@
 public class runner {
-    public static void main (String [] args)
-    {
-        test listA = new test();
-        test listB = new test();
-        //test listC = new test();
-
-        listA.add(3);
-        listA.add(8);
-        listA.add(3);
-        
-        listB.add(5);
-        listB.add(3);
-        listB.add(5);
-
+    public static void main(String[] args) {
+        stackMin listA = new stackMin();
+        listA.push(4);
+        listA.push(2);
+        listA.push(1);
+        listA.push(4);
         listA.print();
-        listB.print();
+
+        System.out.println("listA pop = " + listA.pop());
+        listA.print();
+
+        System.out.println("listA min = " + listA.min());
     }
 }
