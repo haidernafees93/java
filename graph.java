a8c81c8db5d5227855cbdca595576705dee5c521
Haider Nafees <haidernafees93@gmail.com>
2020-07-01 20:16:21 -0400

    undirected graph print

--- graph.java
+++ graph.java
@@ -1,4 +1,6 @@
-import java.util.*; 
+import java.util.*;
+import java.util.Queue;
+import java.util.LinkedList;
   
 class graph { 
       
@@ -7,6 +9,32 @@ class graph {
     static void addEdge(ArrayList<ArrayList<Integer>> adj, int u, int v){
         adj.get(u).add(v);
         adj.get(v).add(u);
+    }
+
+    static void bfs(ArrayList<ArrayList<Integer>> adj){
+        boolean visited[] = new boolean[adj.size()];
+        for(int i = 0; i < adj.size(); i++){
+            if(visited[i]==true){
+
+            }
+            else if(adj.get(i).size() == 0){
+
+            }
+            else{
+                visited[i] = true;
+                System.out.print(i + "  ");
+                for(int j = 0; j < adj.get(i).size(); j++){
+                    if(visited[adj.get(i).get(j)]==true){
+
+                    }
+                    else{
+                        visited[adj.get(i).get(j)] = true;
+                        System.out.print(adj.get(i).get(j) + "  ");
+                    }
+                }
+            }
+        }
+        System.out.println();
     }
 
     static void print(ArrayList<ArrayList<Integer>> adj){
@@ -29,12 +57,12 @@ class graph {
 
         addEdge(adj, 0, 1); 
         addEdge(adj, 0, 4); 
-        addEdge(adj, 1, 2); 
+        // addEdge(adj, 1, 2); 
         addEdge(adj, 1, 3); 
         addEdge(adj, 1, 4); 
         addEdge(adj, 2, 3); 
         addEdge(adj, 3, 4); 
 
-        print(adj);
+        bfs(adj);
     }
 } 
